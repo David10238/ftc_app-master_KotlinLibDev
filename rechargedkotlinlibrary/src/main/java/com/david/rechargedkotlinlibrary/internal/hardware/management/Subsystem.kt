8691ea6844757be8c07aff5e5d630cb7b1e484ab
David Lukens <davidqcy02@gmail.com>
2018-08-03 11:59:04 -0500

    added analog and digital devices

--- rechargedkotlinlibrary/src/main/java/com/david/rechargedkotlinlibrary/internal/hardware/management/Subsystem.kt
+++ rechargedkotlinlibrary/src/main/java/com/david/rechargedkotlinlibrary/internal/hardware/management/Subsystem.kt
@@ -4,6 +4,6 @@ package com.david.rechargedkotlinlibrary.internal.hardware.management
  * Created by David Lukens on 8/2/2018.
  */
 abstract class Subsystem (robot:RobotTemplate){
-    internal val hMap = robot.hMap
+    protected val hMap = robot.hMap
     abstract fun start()
 }
