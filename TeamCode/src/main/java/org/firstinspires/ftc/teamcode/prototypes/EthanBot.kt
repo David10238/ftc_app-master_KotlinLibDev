b71adaa09362c79b24903079a9ef5c876832ae9e
David Lukens <davidqcy02@gmail.com>
2018-09-11 14:29:13 -0500

    fixed EthanBot hardware class

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/prototypes/EthanBot.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/prototypes/EthanBot.kt
@@ -1,8 +1,10 @@
 package org.firstinspires.ftc.teamcode.prototypes
 
+import com.acmerobotics.roadrunner.drive.Drive
 import com.david.rechargedkotlinlibrary.internal.hardware.management.RobotTemplate
 import com.david.rechargedkotlinlibrary.internal.opMode.RechargedLinearOpMode
 import com.david.rechargedkotlinlibrary.internal.util.AutoTransitionerKotlin
+import com.qualcomm.hardware.bosch.BNO055IMU
 
 /**
  * Created by David Lukens on 9/10/2018.
@@ -10,4 +12,6 @@ import com.david.rechargedkotlinlibrary.internal.util.AutoTransitionerKotlin
 class EthanBot(opmode:RechargedLinearOpMode<EthanBot>) : RobotTemplate(opmode, arrayOf("hub1")){
     val drive = EthanDrive(robot = this)
     override fun autoPostInit() = AutoTransitionerKotlin.transitionOnStop(opMode, EthanTele.NAME)
+    override fun getDrive():Drive = drive
+    override fun getGyro():BNO055IMU = hMap.get(BNO055IMU::class.java, "imu")
 }
